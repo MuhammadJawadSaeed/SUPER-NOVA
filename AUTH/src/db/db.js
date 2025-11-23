9f59be113960db0a95e97be052c088490ecb3580
MuhammadJawadSaeed <jawadsaeed917@gmail.com>
2025-11-24 03:15:23 +0500

    Super nova authentication, user register and user login api created

---
+++ AUTH/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+async function connectDB(){
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Database is connected successfully');
+    
+  } catch (err) {
+    console.log('Database connection failed', err);
+  }
+}
+module.exports = connectDB;
---
+++ AUTH/src/routes/auth.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const validators = require('../middleware/validator.middleware')
+const authController = require('../controllers/auth.controller')
+
+
+const router = express.Router();
+
+router.post("/register", validators.registerUserValidations, authController.registerUser);
+
+
+router.post('/login', validators.loginUserValidations, authController.loginUser);
+
+module.exports = router;
