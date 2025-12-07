8c64d4ec6730111622179aa7e750288f1bfc5876
MuhammadJawadSaeed <jawadsaeed917@gmail.com>
2025-12-08 01:35:25 +0500

    cart service completed

--- cart/src/routes/cart.routes.js
+++ cart/src/routes/cart.routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const createAuthMiddleware = require('../middlewares/auth.middleware');
-const cartController = require('../controllers/cart.controller');
+const createAuthMiddleware = require("../middlewares/auth.middleware");
+const cartController = require("../controllers/cart.controller");
 const validation = require("../middlewares/validation.middleware");
 
 const router = express.Router();
 
+router.get("/", createAuthMiddleware(["user"]), cartController.getCart);
 
-router.post('/items', createAuthMiddleware(['user']), validation.validateAddItemToCart, cartController.addItemToCart);
+router.post(
+  "/items",
+  createAuthMiddleware(["user"]),
+  validation.validateAddItemToCart,
+  cartController.addItemToCart
+);
 
+router.patch(
+  "/items/:productId",
+  validation.validateUpdateCartItem,
+  createAuthMiddleware(["user"]),
+  cartController.updateItemQuantity
+);
 
 module.exports = router;
